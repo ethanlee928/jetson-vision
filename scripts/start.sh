d72301c61ec6ecbeb9219f369e3d115d1fbfb302
ethanlee928 <ethan2000.el@gmail.com>
2024-06-14 14:24:08 +0800

    updated start scripts, added network folder for mounting pre-trained models

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+modelDir="${modelDir:-${PWD}/networks}"
+
 docker run -it --rm --network host -v ${PWD}:/app/ \
-    -v ${PWD}/networks:/jetson-inference/data/networks \
+    -v ${modelDir}:/jetson-inference/data/networks \
     -v /run/jtop.sock:/run/jtop.sock \
     -v /dev/video0:/dev/video0 \
     --device /dev/video0 \
