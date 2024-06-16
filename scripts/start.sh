c67a8d20f70d5e6b2444d4f10bfec790a8fef89a
ethanlee928 <ethan2000.el@gmail.com>
2024-06-16 15:10:42 +0800

    restructure project

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,6 +6,8 @@ docker run -it --rm --network host -v ${PWD}:/app/ \
     -v ${modelDir}:/jetson-inference/data/networks \
     -v /run/jtop.sock:/run/jtop.sock \
     -v /dev/video0:/dev/video0 \
+    -v /tmp/.X11-unix/:/tmp/.X11-unix \
+    -e DISPLAY=$DISPLAY \
     --device /dev/video0 \
     -w /app/ \
     jetson-vision \
