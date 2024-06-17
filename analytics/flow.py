844155ae6ffb956b6e564c444b69e87ab002af7d
ethanlee928 <ethan2000.el@gmail.com>
2024-06-17 17:28:44 +0800

    updated flow to use LineZoneAnnotator

--- analytics/flow.py
+++ analytics/flow.py
@@ -19,6 +19,7 @@ if __name__ == "__main__":
     start = sv.Point(1280 // 2, 0)
     end = sv.Point(1280 // 2, 720)
     line_zone = sv.LineZone(start=start, end=end)
+    line_zone_annotator = sv.LineZoneAnnotator()
 
     tracker = sv.ByteTrack()
 
@@ -29,15 +30,17 @@ if __name__ == "__main__":
             continue
 
         detections = net.Detect(img, overlay=opt.overlay)
+        img_np = jetson_utils.cudaToNumpy(img)
         sv_detections = to_sv_detections(detections)
         if sv_detections:
             sv_detections = tracker.update_with_detections(sv_detections)
             line_zone.trigger(sv_detections)
-            jetson_utils.Log.Success(
+            img_np = line_zone_annotator.annotate(img_np, line_counter=line_zone)
+            jetson_utils.Log.Info(
                 f"In: {line_zone.in_count}, Out: {line_zone.out_count}"
             )
-
-        output.Render(img)
+        img_cuda = jetson_utils.cudaFromNumpy(img_np)
+        output.Render(img_cuda)
 
         if not input.IsStreaming() or not output.IsStreaming():
             break
